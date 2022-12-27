73f1f532e76c63e045ab5ed8e1fb6c3f754f8608
victormontoya1991 <disvictormontoya@gmail.com>
2022-12-27 11:11:10 -0500

    carga header

---
+++ react-retotecnicovictormontoya/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import LogoBrand from '../../assets/img/Logo-web-DIS.png';
+
+export const Header = () => {
+    return(
+        <header className= "headerPage">
+            <div className='LogoBrand'>
+                <img src={LogoBrand} alt="Reto Tecnico"></img>
+            </div>
+            <h1 className= "titleProyect"> Bandeja de pagos</h1>
+        </header>
+    )
+}
